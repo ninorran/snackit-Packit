8f7ee587527bca8077ae0e251df6719d0c4c016e
himanashuraj-webkul <himanshuraj.odoo209@webkul.in>
2026-06-05 19:44:26 +0530

    [UPD] payment_sknpay

--- payment_sknpay/__init__.py
+++ payment_sknpay/__init__.py
@@ -10,11 +10,19 @@
 from . import controllers
 from . import models
 
+from odoo.fields import Command
 from odoo.addons.payment import setup_provider, reset_payment_provider
 
 
 def post_init_hook(env):
     setup_provider(env, 'sknpay')
+    providers = env['payment.provider'].with_context(active_test=False).search(
+        [('code', '=', 'sknpay')]
+    )
+    methods = env['payment.method'].with_context(active_test=False).search(
+        [('code', 'in', ['card', 'visa', 'mastercard'])]
+    )
+    providers.write({'payment_method_ids': [Command.set(methods.ids)]})
 
 
 def uninstall_hook(env):
